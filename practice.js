fefb8bc671e6724d5b66acf8ec436aa33de56d44
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-07-07 10:23:00 -0600

    Finish testing

--- practice.js
+++ practice.js
@@ -1,10 +1,35 @@
-function findRemainder() {
-
+function findRemainder(num1, num2) {
+  return num1 % num2
 }
+function startGPS(currentPoint, newPoint) {
+  if (currentPoint === newPoint) {
+    return `You are already at the ${currentPoint}!  Would you like to go somewhere else?`
+  }
+ else {return `Taking you from ${currentPoint} to ${newPoint}!  Getting directions now!`
+}
+};
+
+function createUser(name1, name2, birthDay){
+  return {
+    firstName: name1,
+    lastName: name2,
+    birthDate: birthDay,
+    onlineStatus: false
+  }
+};
+
+function makePizza(pizza, topping){
+if (pizza.toppings.length <= 4){
+    pizza.toppings.push(topping)
+    pizza.cooked = true
+}
+}
+
+
 
 module.exports = {
   findRemainder,
-  // startGPS,
-  // createUser,
-  // makePizza
+  startGPS,
+  createUser,
+  makePizza
 }
